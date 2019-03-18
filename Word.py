7630ea12ddc6da8f55e07119ef339824725614d0
abarnaramaraj <47477481+abarnaramaraj@users.noreply.github.com>
2019-03-18 15:16:34 +0530

    Create Word.py

---
+++ Word.py
@@ -0,0 +1,7 @@
+def return_big digit(number,devidedby):
+prrdef={0:"zero"1:"one",2:"two",3:"three"}
+if devideby in predef.key():
+return predef[number/devideby]+" "predef[devideby]
+else
+devideby/10
+return handle_upto_99 [number/devideby]+" "+predef[devideby]
